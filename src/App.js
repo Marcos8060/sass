ab9a019bafbebd5d9660aff55acfd72a226289c2
Marcos8060 <marcosgav80@gmail.com>
2022-07-13 12:57:23 +0300

    shared mixin functions between sass files

--- src/App.js
+++ src/App.js
@@ -2,10 +2,17 @@ import './style/style.css'
 
 function App() {
   return (
-    <div className="header">
-      <h1>CSS Super power</h1>
-      <button>Hello</button>
-    </div>
+    <>
+      <div className="header">
+        <h1>CSS Super power</h1>
+        <button>Hello</button>
+      </div>
+      
+      <div className="contact">
+        <h2>I am the contact section</h2>
+      </div>
+    </>
+    
   );
 }
 
